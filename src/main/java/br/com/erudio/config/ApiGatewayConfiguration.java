f7bf99a83add99da1b6999a5c136d5b9385ae5fb
Stanley <stanleysantos@gmail.com>
2025-10-17 21:29:28 -0300

    Add routes to loadbalancers dos microservicos

--- src/main/java/br/com/erudio/config/ApiGatewayConfiguration.java
+++ src/main/java/br/com/erudio/config/ApiGatewayConfiguration.java
@@ -11,12 +11,19 @@ public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
         return builder.routes()
-                .route(p -> p.path("/get")
+                /* exemplos */
+                /*.route(p -> p.path("/get")
                         .filters( f -> f
                                 .addRequestHeader("Hello", "World")
                                 .addRequestParameter("Hello", "World")
                         )
-                        .uri("http://httpbin.org:80"))
+                        .uri("http://httpbin.org:80"))*/
+                .route(p -> p.path("/book-service/**")
+                            .uri("lb://book-service") /*lb: loadbalancer*/
+                )
+                .route(p -> p.path("/exchange-service/**")
+                        .uri("lb://exchange-service") /*lb: loadbalancer*/
+                )
                 .build();
     }
 }
