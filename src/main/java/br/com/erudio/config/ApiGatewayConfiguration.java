91c279c9cca170bd2fc016ca23f36542a8b80db0
Stanley <stanleysantos@gmail.com>
2025-10-17 21:25:04 -0300

    Add router

---
+++ src/main/java/br/com/erudio/config/ApiGatewayConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.erudio.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters( f -> f
+                                .addRequestHeader("Hello", "World")
+                                .addRequestParameter("Hello", "World")
+                        )
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+}
